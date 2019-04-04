e4880591a8a5b55a0ed6e6b8303722f92c49ef0c
Erinlampa <43894479+Erinlampa@users.noreply.github.com>
2019-04-04 18:34:11 -0500

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY = "pk.eyJ1IjoiZXJpbmxhbXBhIiwiYSI6ImNqdGdnYXZndDIzbTc0OXA5d2g2enI4M28ifQ.yUvUB1BNvhyLugg3k8fV4w";
